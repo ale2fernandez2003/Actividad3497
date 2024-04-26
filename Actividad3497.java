9986dfb2cf85f0977004c7f47786bc688c1daccf
Alejandro <ale2fernandez2003@gmail.com>
2024-04-26 12:59:02 +0200

    commit inicial

---
+++ Actividad3497.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Actividad3497 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Actividad3497
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<String> palabra;
+
+    /**
+     * Constructor for objects of class Actividad3497
+     */
+    public Actividad3497()
+    {
+        // initialise instance variables
+        palabra = new ArrayList();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public ArrayList<String> devolverPalabraMasLarga(ArrayList<String> palabras)
+    {
+        ArrayList<String> palabrasMasLargas = new ArrayList<>();
+        return palabrasMasLargas;
+    }
+}
